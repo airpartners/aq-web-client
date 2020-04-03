44b5492fcc4cf7ec4c8d78cf5950bd43338dd7d6
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2020-04-03 19:46:22 -0400

    Fetch device data on tab changed

--- src/App.js
+++ src/App.js
@@ -21,8 +21,12 @@ import DetailIcon from '@material-ui/icons/BubbleChart';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import HelpIcon from '@material-ui/icons/Help';
 import InfoIcon from '@material-ui/icons/Info';
+import * as DBHelper from './DBHelper';
 
 const drawerWidth = 240;
+const deviceList = ['SN000-045', 'SN000-046', 'SN000-049', 'SN000-062', 'SN000-067', 'SN000-072'];
+const aboutUsText = 'About Us';
+const qaaText = 'Q&A';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -57,15 +61,44 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-function ResponsiveDrawer(props) {
+function App(props) {
     const {container} = props;
     const classes = useStyles();
     const theme = useTheme();
     const [mobileOpen, setMobileOpen] = React.useState(false);
-    const [value, setValue] = React.useState(0);
-
+    const [tabValue, setTabValue] = React.useState(0);
+    const [currentPath, setCurrentPath] = React.useState(deviceList[0]);
+    const [currentDevice, setCurrentDevice] = React.useState({name: deviceList[0], data: {}});
+    const [deviceData, setDeviceData] = React.useState({});
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
+    };
+    const fetchDeviceData = (device) => {
+        DBHelper.getData(device).then((data) => {
+            let newState = deviceData;
+            newState[device] = data;
+            setDeviceData(newState);
+        }).catch((e) => {
+            console.log(e);
+        })
+    };
+    const updatePage = (path) => {
+        // Update current device
+        if (deviceList.includes(path) && path !== currentDevice.name) {
+            // Fetch device data if needed
+            if (!(path in deviceData)) {
+                fetchDeviceData(path);
+                console.log(deviceData);
+            }
+            setCurrentDevice({name: path, data: deviceData[path]});
+        }
+    };
+    const setPath = (event, path) => {
+        event.stopPropagation();
+        if (currentPath !== path) {
+            updatePage(path);
+            setCurrentPath(path);
+        }
     };
 
     const drawer = (
@@ -73,8 +106,8 @@ function ResponsiveDrawer(props) {
             <div className={classes.toolbar}/>
             <Divider/>
             <List>
-                {['SN000-045', 'SN000-046', 'SN000-049', 'SN000-062', 'SN000-067', 'SN000-072'].map((text) => (
-                    <ListItem button key={text}>
+                {deviceList.map((text) => (
+                    <ListItem button key={text} selected={currentPath === text} onClick={(e) => setPath(e, text)}>
                         <ListItemIcon><DeviceIcon/></ListItemIcon>
                         <ListItemText primary={text}/>
                     </ListItem>
@@ -82,13 +115,14 @@ function ResponsiveDrawer(props) {
             </List>
             <Divider/>
             <List>
-                <ListItem button key='About Us'>
+                <ListItem button key={aboutUsText} selected={currentPath === aboutUsText}
+                          onClick={(e) => setPath(e, aboutUsText)}>
                     <ListItemIcon><InfoIcon/></ListItemIcon>
-                    <ListItemText primary='About Us'/>
+                    <ListItemText primary={aboutUsText}/>
                 </ListItem>
-                <ListItem button key='Q&A'>
+                <ListItem button key={qaaText} selected={currentPath === qaaText} onClick={(e) => setPath(e, qaaText)}>
                     <ListItemIcon><HelpIcon/></ListItemIcon>
-                    <ListItemText primary='Q&A'/>
+                    <ListItemText primary={qaaText}/>
                 </ListItem>
             </List>
         </div>
@@ -115,7 +149,7 @@ function ResponsiveDrawer(props) {
             </AppBar>
 
             {/* Responsive Drawer */}
-            <nav className={classes.drawer} aria-label="mailbox folders">
+            <nav className={classes.drawer} aria-label="device list">
                 {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
                 <Hidden smUp implementation="css">
                     <Drawer
@@ -125,7 +159,7 @@ function ResponsiveDrawer(props) {
                         open={mobileOpen}
                         onClose={handleDrawerToggle}
                         classes={{paper: classes.drawerPaper,}}
-                        ModalProps={{keepMounted: true,}}>// Better open performance on mobile.
+                        ModalProps={{keepMounted: true,}}>
                         {drawer}
                     </Drawer>
                 </Hidden>
@@ -145,13 +179,23 @@ function ResponsiveDrawer(props) {
                 <div>
                     <div className={classes.content} style={{marginBottom: "56px"}}>
                         <Typography paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum porttitor justo,
+                            vel efficitur justo bibendum a. Ut mollis vel nulla quis pretium. Vestibulum aliquam dapibus
+                            lectus, in venenatis ex dictum iaculis. Nulla fermentum ornare tortor, vitae ullamcorper
+                            sem. Phasellus est ligula, auctor vel ex in, finibus tincidunt nisl. Suspendisse dictum ex a
+                            velit fringilla, sit amet ultricies justo fringilla. Donec consectetur magna eget orci
+                            porttitor vestibulum. Donec at luctus lorem. Fusce euismod commodo massa, non ornare nisl
+                            cursus sed. Nam bibendum justo odio, non hendrerit lectus dictum at. Cras eros orci,
+                            faucibus commodo maximus ac, laoreet a lacus. Nam pulvinar aliquet efficitur. Pellentesque
+                            in mattis eros. Mauris vitae metus sapien. Phasellus non turpis sed nisl dignissim
+                            efficitur. Phasellus pulvinar turpis at ultrices tempor.
                         </Typography>
                     </div>
                     <BottomNavigation
                         style={{position: "fixed", width: `calc(100% - ${drawerWidth}px)`, bottom: 0}}
-                        value={value}
+                        value={tabValue}
                         onChange={(event, newValue) => {
-                            setValue(newValue);
+                            setTabValue(newValue);
                         }}
                         showLabels
                         className={classes.root}>
@@ -165,4 +209,4 @@ function ResponsiveDrawer(props) {
     );
 }
 
-export default ResponsiveDrawer;
+export default App;
