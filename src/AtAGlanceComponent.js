a4c5e6fa52add709d9c5f5d4aaa9989d382dce9c
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2020-04-17 03:29:45 -0400

    Add CloudSVG component

--- src/AtAGlanceComponent.js
+++ src/AtAGlanceComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
+import CloudSVG from "./assets/svg/CloudSVG";
 
 const useStyles = makeStyles((theme) => ({}));
 
@@ -7,12 +8,47 @@ function AtAGlanceComponent(props) {
     const {t} = props;
     const classes = useStyles();
     const {device} = props;
+    const getAirQuality = (device) => {
+        // TODO: Update this func to work with real data
+        let r = Math.floor((Math.random() * 3) + 1);
+        switch (r) {
+            case 1:
+                return t('Bad');
+            case 2:
+                return t('Fine');
+            default:
+                return t('Good');
+        }
+    };
+    const airQuality = getAirQuality(device);
+    const getColor = (indicator) => {
+        switch (indicator) {
+            case t('Bad'):
+                return "#B53F3F";
+            case t('Fine'):
+                return "#F2C94C";
+            default:
+                return "#3FB571";
+        }
+    };
+    const getRecommendation = (indicator) => {
+        switch (indicator) {
+            case t('Bad'):
+                return t('Recommendation.Bad');
+            case t('Fine'):
+                return t('Recommendation.Fine');
+            default:
+                return t('Recommendation.Good');
+        }
+    };
     return (
         <div>
             {device.data.data &&
             <div>
                 <h2>{t('Now')}</h2>
+                <CloudSVG fill={getColor(airQuality)}/>
                 <h2>{t('Recommendations')}</h2>
+                <p>{getRecommendation(airQuality)}</p>
             </div>}
         </div>);
 }
