7ff699716813b564f1df18f1fb0105df56499bd1
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2020-04-22 18:23:19 -0400

    Surround Bubble with Grid to better support responsiveness

--- src/AirQualityComponent.js
+++ src/AirQualityComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import BubbleSVG from "./assets/svg/BubbleSVG";
 import Colors from "./assets/Colors";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({}));
 
@@ -14,22 +15,28 @@ function AirQualityComponent(props) {
             {device.data &&
             <div>
                 <h2>{t('Air Quality')}</h2>
-                <BubbleSVG color={Colors.red} percent={65}
-                           standardText1="35 ug/m3" standardText2="EPA 24 hour" standardText3="standard"
-                           currentText="37 ug/m3" pollutantText="PM2.5"
-                           currentTextLength={55} pollutantTextLength={45}/>
-                <BubbleSVG color={Colors.yellow} percent={60}
-                           standardText1="35 ppm" standardText2="EPA 1 hour" standardText3="standard"
-                           currentText="36 ppm" pollutantText="CO"
-                           currentTextLength={50} pollutantTextLength={20}/>
-                <BubbleSVG color={Colors.green} percent={53}
-                           standardText1="105 ppb" standardText2="EPA 8 hour" standardText3="standard"
-                           currentText="100 ppb" pollutantText="NO2"
-                           currentTextLength={50} pollutantTextLength={28}/>
-                <BubbleSVG color={Colors.green} percent={45}
-                           standardText1="69 ppb" standardText2="EPA 8 hour" standardText3="standard"
-                           currentText="69 ppb" pollutantText="O3"
-                           currentTextLength={50} pollutantTextLength={18}/>
+                <Grid container>
+                    <Grid>
+                        <BubbleSVG color={Colors.red} percent={65}
+                                   standardText1="35 ug/m3" standardText2="EPA 24 hour" standardText3="standard"
+                                   currentText="37 ug/m3" pollutantText="PM2.5"
+                                   currentTextLength={55} pollutantTextLength={45}/>
+                        <BubbleSVG color={Colors.yellow} percent={60}
+                                   standardText1="35 ppm" standardText2="EPA 1 hour" standardText3="standard"
+                                   currentText="36 ppm" pollutantText="CO"
+                                   currentTextLength={50} pollutantTextLength={20}/>
+                    </Grid>
+                    <Grid>
+                        <BubbleSVG color={Colors.green} percent={53}
+                                   standardText1="105 ppb" standardText2="EPA 8 hour" standardText3="standard"
+                                   currentText="100 ppb" pollutantText="NO2"
+                                   currentTextLength={50} pollutantTextLength={28}/>
+                        <BubbleSVG color={Colors.green} percent={45}
+                                   standardText1="69 ppb" standardText2="EPA 8 hour" standardText3="standard"
+                                   currentText="69 ppb" pollutantText="O3"
+                                   currentTextLength={50} pollutantTextLength={18}/>
+                    </Grid>
+                </Grid>
             </div>}
         </div>);
 }
