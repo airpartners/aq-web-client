9e5087fc9ac814b29490ea35cfe50909f3063442
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2020-04-21 20:09:42 -0400

    Document Utils.js

--- src/Utils.js
+++ src/Utils.js
@@ -5,62 +5,91 @@ export const deviceInitData = {
         id: 'SN000-045',
         name: 'St Andrew Road, EB',
         data: null,
+        meta: null,
         lastUpdated: null,
     },
     'SN000-046': {
         id: 'SN000-046',
         name: 'Everett Street, EB',
         data: null,
+        meta: null,
         lastUpdated: null,
     },
     'SN000-049': {
         id: 'SN000-049',
         name: 'Elmer Ave, Winthrop',
         data: null,
+        meta: null,
         lastUpdated: null,
     },
     'SN000-062': {
         id: 'SN000-062',
         name: 'Sumner, EB',
         data: null,
+        meta: null,
         lastUpdated: null,
     },
     'SN000-067': {
         id: 'SN000-067',
         name: 'Bay View Ave, Winthrop',
         data: null,
+        meta: null,
         lastUpdated: null,
     },
     'SN000-072': {
         id: 'SN000-072',
         name: 'Anna Voy, EB',
         data: null,
+        meta: null,
         lastUpdated: null,
     }
 };
+
+/**
+ * Set tab name based on id
+ * @param tabId: 0, 1 -> Home, Map
+ * @param t: translation object
+ * @returns {string} translated string for Home and Map
+ */
 export const getTabName = (tabId, t) => {
     switch (tabId) {
-        case 1:
-            return t('BottomNav.Map');
-        case 2:
-            return t('BottomNav.Detail');
-        default:
+        case 0:
             return t('BottomNav.Home');
+        default:
+            return t('BottomNav.Map');
     }
 };
+
+/**
+ * Get tab id based on tab name
+ * @param tabName: Home, Map -> 0, 1
+ * @param t: translation object
+ * @returns {number}
+ */
 export const getTabId = (tabName, t) => {
     switch (tabName) {
-        case t('BottomNav.Map'):
-            return 1;
-        case t('BottomNav.Detail'):
-            return 2;
-        default:
+        case t('BottomNav.Home'):
             return 0;
+        default:
+            return 1;
     }
 };
+
+/**
+ * Check whether a url is a device path (aka. containing 'SN')
+ * @param path: a url path (i.e. /SN000-049/Home)
+ * @returns {boolean}: true if device path
+ */
 export const isDevicePath = (path) => {
     return path.includes('SN') || path === '';
 };
+
+/**
+ * Get device name and tab id from a url path
+ * @param path: a url path (i.e. /SN000-049/Home)
+ * @param t: translation object to retrieve tab name
+ * @returns {[string, number]}: device name, tab id
+ */
 export const getDeviceMetaData = (path, t) => {
     let metaData = path.split('/').filter(el => el); // Split and filter out null/empty string
     let deviceName = metaData.length > 0 ? metaData[0] : deviceList[0];
@@ -73,6 +102,15 @@ export const getDeviceMetaData = (path, t) => {
     }
     return [deviceName, getTabId(tabName, t)];
 };
+
+/**
+ * Check whether we should fetch new data. Update when
+ * - Data has just been initialized (lastUpdated == null)
+ * - Last updated was 5 minutes ago or longer
+ * @param deviceDict: dictionary of our device data
+ * @param deviceName: name of the current device
+ * @returns {boolean}: true if update is needed
+ */
 export const needUpdate = (deviceDict, deviceName) => {
     let lastUpdated = deviceDict[deviceName].lastUpdated;
     if (!lastUpdated)
