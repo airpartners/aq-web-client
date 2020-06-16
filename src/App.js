9fcd8c21af606377d7ed451e820a21a958601874
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2020-06-16 13:41:15 -0400

    initial firebase setup

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import { withNamespaces } from 'react-i18next';
 import { deviceList, deviceInitData, isDevicePath, needUpdate, getDeviceMetaData } from "./Utils";
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Colors from "./assets/Colors";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 // Create our own theme
 const theme = createMuiTheme({
@@ -121,6 +124,29 @@ function App(props) {
                         <Route path={process.env.PUBLIC_URL + '/'} render={() => devicePage()} />
                     </Switch>
                 </main>
+                {/* The core Firebase JS SDK is always required and must be listed first */}
+                <script src="https://www.gstatic.com/firebasejs/7.15.1/firebase-app.js"></script>
+
+                {/* TODO: Add SDKs for Firebase products that you want to use
+                    https://firebase.google.com/docs/web/setup#available-libraries */}
+                <script src="https://www.gstatic.com/firebasejs/7.15.1/firebase-analytics.js"></script>
+
+                <script>
+                // Your web app's Firebase configuration
+                var firebaseConfig = {
+                    apiKey: "AIzaSyByyEO8A3-mCIjcTYoVXC6_9C2NGskBvzA",
+                    authDomain: "ade-airparnters.firebaseapp.com",
+                    databaseURL: "https://ade-airparnters.firebaseio.com",
+                    projectId: "ade-airparnters",
+                    storageBucket: "ade-airparnters.appspot.com",
+                    messagingSenderId: "831140017575",
+                    appId: "1:831140017575:web:33b93a1383a9367ae6f282",
+                    measurementId: "G-4W9PM87VRH"
+                };
+                // Initialize Firebase
+                firebase.initializeApp(firebaseConfig);
+                firebase.analytics();
+                </script>
             </div>
         </MuiThemeProvider>
     );
