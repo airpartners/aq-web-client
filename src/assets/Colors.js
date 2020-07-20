3ba1d3219f42dc2d130505af2199fe6a2456eff2
Lucky <ljordan51@gmail.com>
2020-07-20 16:55:54 -0700

    updated primary color to match airpartners.org dark blue, removed unused red/yello/green

--- src/assets/Colors.js
+++ src/assets/Colors.js
@@ -1,9 +1,7 @@
 const Colors = {
-    red: "#B53F3F",
-    yellow: "#F2C94C",
-    green: "#3FB571",
-    primaryColor: "#303F9F",
-    secondaryColor: "#eb861a",
+    primaryColorLight: "#69C5ED",
+    primaryColor: "#1976A1",
+    secondaryColor: "#EB861A",
     grey: "#C5CAE9",
 }
 
