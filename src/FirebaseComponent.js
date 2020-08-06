3a411a4556cf53a26e126e8eff253f711f1fa59e
Lucky <ljordan51@gmail.com>
2020-08-05 17:20:06 -0700

    updated to use proper sensors, updated geo data

--- src/FirebaseComponent.js
+++ src/FirebaseComponent.js
@@ -15,28 +15,6 @@ const firebaseConfig = {
 }
 
 export const getData = async (deviceId = 'SN000-072') => {
-    switch (deviceId) {
-        case 'SN000-045':
-            deviceId = 'SN000-062';
-            break;
-        case 'SN000-046':
-            deviceId = 'SN000-067';
-            break;
-        case 'SN000-049':
-            deviceId = 'SN000-075';
-            break;
-        case 'SN000-062':
-            deviceId = 'SN000-088';
-            break;
-        case 'SN000-067':
-            deviceId = 'SN000-089';
-            break;
-        case 'SN000-072':
-            deviceId = 'SN000-094';
-            break;
-        default:
-            deviceId = 'SN000-088';
-    }
     const latestSnapshot = await firebase.app().database().ref(`/${deviceId}/latest`).once('value');
     const graphSnapshot = await firebase.app().database().ref(`/${deviceId}/graph`).once('value');
     return { latest: latestSnapshot.val(), graph: graphSnapshot.val() };
