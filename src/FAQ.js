c3dede48a11834b1660d9836a3a2a7e245493d65
Dieter Brehm <inkering@gmail.com>
2021-03-08 12:35:43 -0500

    rough draft content for layout testing

--- src/FAQ.js
+++ src/FAQ.js
@@ -54,25 +54,30 @@ const useStyles = makeStyles((theme) => ({
 /* replace with strings props  */
 const FAQData = [
 		{
-				title: "CO - Carbon Monoxide",
-				shortText: "Carbon monoxide (CO) is a colorless, odorless, tasteless gas produced by burning gasoline, wood, propane, charcoal or other fuel.",
-				longText: "Carbon monoxide (CO) is a colorless and odorless gas. It results from the incomplete combustion of carbon-containing fuels such as natural gas, gasoline, or wood, and is emitted by a wide variety of combustion sources, including  motor vehicles, power plants, wildfires, and incinerators"
+				title: "What are ultrafine particles?",
+				shortText: "Learn more about particulates in the air we breathe.",
+				longText: "Ultrafine particles are particles that are 100 times smaller than particles regulated by the EPA. They are emitted when jet fuel is burned. They are dangerous because they can go farther into the lungs and cross the blood brain behavior."
 		},
 		{
-				title: "NO2 - Nitrogen Dioxide",
-				shortText: "Nitrogen dioxide (NO2) is part of a group of gaseous air pollutants produced as a result of road traffic and other fossil fuel combustion processes. Its presence in air contributes to the formation and modification of other air pollutants, such as ozone and particulate matter, and to acid rain.",
-				longText: "Nitrogen oxides are a mixture of gases that are composed of nitrogen and oxygen. Two of the most toxicologically significant nitrogen oxides are nitric oxide and nitrogen dioxide; both are nonflammable and colorless to brown at room temperature. \n This is a sharp sweet-smelling gas at room temperature, whereas nitrogen dioxide has a strong, harsh odor and is a liquid at room temperature, becoming a reddish-brown gas above 70 EF. \n Nitrogen oxides are released to the air from the exhaust of motor vehicles, the burning of coal, oil, or natural gas, and during processes such as arc welding, electroplating, engraving, and dynamite blasting. They are also produced commercially by reacting nitric acid with metals or cellulose. Nitrogen oxides are used in the production of nitric acid, lacquers, dyes, and other chemicals. Nitrogen oxides are also used in rocket fuels, nitration of organic chemicals, and the manufacture of explosives."
+			title: "What does ppb mean when referring to pollutant concentration?",
+			shortText: "Notes on units and how air quality is measured.",
+			longText: "PPB stands for “parts per billion.” Concentration in parts per billion refers to how many pollutant molecules there are for every billion air molecules in a particular volume.",
 		},
 		{
-				title: "NO (ppb)",
-				shortText: "Nitrogen oxide (NOx) is a chemical compound of oxygen and nitrogen that is formed by reacting with each other during combustion at high temperatures, mainly combustion of fuel such as oil, diesel, gas and organic matter. NOx is a common designation of nitrogen oxides NO and NO2.",
-				longText: "NOx is a term used to describe a mixture of nitric oxide (NO) and nitrogen dioxide (NO2). They are inorganic gases formed by the combination of oxygen with nitrogen from the air. NO is produced in much greater quantities than NO2, but oxidizes to NO2 in the atmosphere. NO2 causes detrimental effects to the bronchial system. NO2 concentrations frequently approach, and sometimes exceed air quality standards in many European cities. NOx is emitted when fuel is being burned e.g. in transport, industrial processes, and power generation."
+			title: "What can I do to reduce my exposure to air pollution?",
+			shortText: "Steps for air quality mitigation.",
+			longText: "There are multiple strategies to reduce your exposure to air pollution. When inside, turning on an air purifier, such as a HEPA Air Purifier will significantly decrease your exposure.\n\n Otherwise, try to reduce your time outside when the air quality is considerably worse than average.",
 		},
 		{
-				title: "O3 - Ozone",
-				shortText: "Ozone (O3) is a highly reactive gas composed of three oxygen atoms. It is both a natural and a man-made product that occurs in the Earth's upper atmosphere and lower atmosphere.  Depending on where it is in the atmosphere, ozone affects life on Earth in either good or bad ways.",
-				longText: "Ozone at ground level – not to be confused with the ozone layer in the upper atmosphere – is one of the major constituents of photochemical smog. It is formed by the reaction with sunlight (photochemical reaction) of pollutants such as nitrogen oxides (NOx) from vehicle and industry emissions and volatile organic compounds (VOCs) emitted by vehicles, solvents and industry. As a result, the highest levels of ozone pollution occur during periods of sunny weather."
+			title:"Why is East Boston particularly susceptible to air pollution?",
+			shortText:"",
+			longText:"Due to its proximity to the airport, East Boston is particularly susceptible to unsafe levels of particulates in the air. When airplanes fly over East Boston to take off and land, a high concentration of particles are released in the air. The particulate concentration is also highly dependent on the direction of the wind.",
 		},
+		{
+			title: "What is Air Partners?",
+			shortText: "",
+			longText: "Air Partners are a team of engineering students from Olin College of Engineering, Babson College and Wellesley College. They partner with community organizations in East Boston to mitigate the community’s exposure to air pollution."
+		}
 ];
 
 function FAQ(props) {
